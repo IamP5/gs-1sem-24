070d310230989a6f03fde3a45c309124341255fc
Tuba <brunogabrielsd@hotmail.com>
2024-06-07 23:58:12 -0300

    contact form

--- main.js
+++ main.js
@@ -4,11 +4,44 @@ const heroTitle = document.querySelector('.hero__title');
 const heroLogo = document.querySelector('.hero__logo');
 
 const phrases = ["Protegendo os Oceanos", "Estimulando a Mudança Global"];
+
+const form = document.getElementById('contactForm');
+const inputs = form.querySelectorAll('input, textarea');
+const errorMessages = {
+  name: 'Por favor, insira seu nome.',
+  email: {
+    valueMissing: 'Por favor, insira seu email.',
+    typeMismatch: 'Por favor, insira um endereço de email válido.',
+  },
+  message: 'Por favor, insira sua mensagem.'
+};
+
 let phraseIndex = 0;
 let letterIndex = 0;
 let isDeleting = false;
 let stopAnimation = false;
-let lastScrollTop = 0;
+
+inputs.forEach(input => {
+  input.addEventListener('input', () => {
+    checkValidity(input);
+  });
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let isValid = true;
+
+  inputs.forEach(input => {
+    if (!checkValidity(input)) {
+      isValid = false;
+    }
+  });
+
+  if (isValid) {
+    alert('Formulário enviado com sucesso!');
+    form.reset();
+  }
+});
 
 function resetAnimation() {
   heroTitle.textContent = '';
@@ -16,7 +49,7 @@ function resetAnimation() {
 
 function startNewPhrase() {
   isDeleting = false;
-  phraseIndex = phraseIndex + 1;
+  phraseIndex = (phraseIndex + 1) % phrases.length;
   if (!stopAnimation) setTimeout(typePhrase, 500);
 }
 
@@ -35,7 +68,7 @@ function animateFadeOut() {
     animateFadeIn();
     stopAnimation = true;
     resetAnimation();
-    heroContent.style.paddingBottom = '20dvh';
+    heroContent.style.paddingBottom = '20vh';
   }, 1000);
 }
 
@@ -85,6 +118,42 @@ function typePhrase() {
   addLetter(currentPhrase);
 }
 
+function scrollToElement(id) {
+  const element = document.getElementById(id);
+  element.scrollIntoView({behavior: "smooth"});
+}
+
+function checkValidity(input) {
+  const error = input.nextElementSibling;
+  const validity = input.validity;
+
+  if (validity.valid) {
+    error.textContent = '';
+    error.style.display = 'none';
+    return true;
+  } else {
+    showError(input, error);
+    return false;
+  }
+}
+
+function showError(input, error) {
+  const type = input.type;
+  const name = input.name;
+
+  if (type === 'email') {
+    if (input.validity.valueMissing) {
+      error.textContent = errorMessages[name].valueMissing;
+    } else if (input.validity.typeMismatch) {
+      error.textContent = errorMessages[name].typeMismatch;
+    }
+  } else {
+    error.textContent = errorMessages[name];
+  }
+  
+  error.style.display = 'block';
+}
+
 window.onload = function () {
   heroTitle.classList.add("typing");
   typePhrase();
@@ -92,6 +161,5 @@ window.onload = function () {
 
 window.onscroll = function() {
   let header = document.querySelector('.header');
-
   header.classList.toggle('sticky', window.scrollY > 0);
 };
